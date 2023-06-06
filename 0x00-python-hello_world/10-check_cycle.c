78e49c3d138886c68f04bab98e547bdb75a0935b
Arnthorny <chidubem.anthony26@gmail.com>
2023-06-05 17:49:09 -0700

    Mandatory and Optional tasks for 0x00. Python - Hello, World

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+/**
+  *check_cycle - check if linked list has a cycle.
+  *@list: A singly linked list
+  *Return: 0 if there's no cycle else return 1.
+ */
+
+int check_cycle(listint_t *list)
+{
+	const listint_t *s = list, *f = list;
+	short int firstMatch = 0;
+
+	if (!list)
+		exit(98);
+
+	while (s)
+	{
+		s = s->next;
+		if (!firstMatch)
+			f = f ? (f->next ? f->next->next : NULL) : NULL;
+		else
+			f = f ? f->next : NULL;
+
+		if (f && f == s)
+		{
+			if (firstMatch)
+				return (0);
+			s = list;
+			firstMatch = 1;
+		}
+
+	}
+	return (1);
+}
+
