835f5c4d0abc7510f889403ea4b937c677ab230b
Arnthorny <chidubem.anthony26@gmail.com>
2023-06-10 03:27:15 -0700

    Changed else condition for 6-print_matrix_integer file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,5 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(row[i]))
             else:
                 print("{:d} ".format(row[i]), end=" ")
-    else:
+    if len(matrix[0]) == 0:
         print()
