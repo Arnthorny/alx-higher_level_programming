6c2e29db2e629e4607effebea92f3b20fbe6ccb4
Arnthorny <chidubem.anthony26@gmail.com>
2023-06-10 14:48:51 -0700

    Optimized code to use sums in checking for palindromes

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -20,36 +20,13 @@ int list_len(listint_t *h)
 }
 
 /**
- * node_data_arr - Function to create array of a linked list data
- * @head: Pointer to head of linked list
- * @len: Size of Array.
- * Return: Pointer to created array
- */
-
-int *node_data_arr(listint_t *head, int len)
-{
-	int *arr_of_data = NULL, i = 0;
-
-	arr_of_data = malloc(sizeof(int) * len);
-	if (!arr_of_data)
-		return (NULL);
-
-	while (head)
-	{
-		arr_of_data[i++] = head->n;
-		head = head->next;
-	}
-	return (arr_of_data);
-}
-
-/**
  * is_palindrome - Function to check if linked list is a palindrome
  * @head: Pointer that points to linked list head
  * Return: 0 if not palindrome else 1
  */
 int is_palindrome(listint_t **head)
 {
-	int len_list, i = 0, *arr_of_d = NULL;
+	int len_list, hlf_len, i = 0, sum_check = 0;
 	listint_t *head_dup;
 
 	if (!head)
@@ -60,16 +37,22 @@ int is_palindrome(listint_t **head)
 	if (len_list == 0)
 		return (1);
 
-	arr_of_d = node_data_arr(head_dup, len_list);
-	if (!arr_of_d)
-		exit(98);
-
-	for (i = 0; i < (len_list / 2); i++)
+	hlf_len = len_list / 2;
+	while (head_dup)
 	{
-		if (!(head_dup->n == arr_of_d[len_list - 1 - i]))
-			return (0);
+
+		if (i < hlf_len)
+			sum_check += head_dup->n;
+		else
+		{
+			if ((len_list % 2) && (i == hlf_len))
+				;
+			else
+				sum_check -= head_dup->n;
+		}
+		i++;
 		head_dup = head_dup->next;
 	}
-	free(arr_of_d);
-	return (1);
+
+	return (sum_check == 0);
 }
