8dd9591a3faf558efdba3bad2527885634428a44
Arnthorny <chidubem.anthony26@gmail.com>
2023-06-27 05:49:17 -0700

    Bug fix so code correctly print max of 10 bytes in Cpython #1 Task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,8 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{}".format(my_list[i]), end='')
             list_len += 1
-        if list_len:
-            print()
+        print()
     except IndexError:
-        pass
+        print()
+        return(list_len)
     return(list_len)
