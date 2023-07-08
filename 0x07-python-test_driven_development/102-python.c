cc805437b371d22615f4aa4580140064d6f0184e
Arnthorny <chidubem.anthony26@gmail.com>
2023-07-07 21:44:07 -0700

    Minor change from wprintf to printf

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -15,7 +15,6 @@
  * You could use freopen to make the stdout stream "not defined"
  * SYNTAX: freopen(NULL, "w", stdout)
  * But on testing this, I find the buffer to be disoriented.
- * So only one I/O function is used: wprintf.
  *
  * Note: printf with the format specifier %ls can be used to
  * print wide character strings too.
@@ -27,29 +26,28 @@ void print_python_string(PyObject *p)
 	Py_ssize_t len, size = 0;
 	wchar_t *str;
 
-	wprintf(L"[.] string object info\n");
+	printf("[.] string object info\n");
 	if (PyUnicode_Check(p))
 		len = PyUnicode_GetLength(p);
 	else
 	{
-		wprintf(L"  [ERROR] Invalid String Object\n");
+		printf("  [ERROR] Invalid String Object\n");
 		return;
 	}
 
-	setlocale(LC_ALL, "");
+	setlocale(LC_ALL, "C.UTF-8");
 
 	str = PyUnicode_AsWideCharString(p, &size);
 
 	if (!PyUnicode_IS_ASCII(p) && PyUnicode_IS_COMPACT(p))
-		wprintf(L"  type: compact unicode object\n");
+		printf("  type: compact unicode object\n");
 	else if (PyUnicode_IS_COMPACT_ASCII(p))
-		wprintf(L"  type: compact ascii\n");
+		printf("  type: compact ascii\n");
 	else
-		wprintf(L"  type: unicode object\n");
+		printf("  type: unicode object\n");
 
-	wprintf(L"  length: %ld\n", len);
-	/*printf("  %ls\n", str);*/
-	wprintf(L"  value: %ls\n", str);
+	printf("  length: %ld\n", len);
+	printf("  value: %ls\n", str);
 
 	PyMem_Free(str);
 }
