3d3162b4f32f7171d4e91c31ec30f98f11e64f94
Arnthorny <chidubem.anthony26@gmail.com>
2023-07-10 22:11:55 -0700

    Added validator to init function

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -17,8 +17,9 @@ class Square(Rectangle):
     """
 
     def __init__(self, size):
-        self.__size = size
+        super().integer_validator("size", size)
         super().__init__(size, size)
+        self.__size = size
 
     def area(self):
         """
