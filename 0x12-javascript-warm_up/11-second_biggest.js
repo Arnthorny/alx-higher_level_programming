29e8f650de6dd6dd088a1686da2ba3335c4d99b8
Arnthorny <chidubem.anthony26@gmail.com>
2023-09-11 22:33:07 -0700

    Use different method for reversals

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,6 +3,7 @@
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const numList = process.argv.slice(2).sort().reverse();
+  const numList = process.argv.slice(2);
+  numList.sort((a, b) => b - a);
   console.log(numList[1]);
 }
