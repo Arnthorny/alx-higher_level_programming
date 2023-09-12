cefc48e3d85bf31d91ccd049efdb3722dc5cf69f
Arnthorny <chidubem.anthony26@gmail.com>
2023-09-12 15:54:49 -0700

    Made change to async version again

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = require('fs/promises');
 
-const data1 = fs.readFileSync(process.argv[2], 'utf-8');
-const data2 = fs.readFileSync(process.argv[3], 'utf-8');
+async function concat () {
+  const data1 = await fs.readFile(process.argv[2], { encoding: 'utf-8' });
+  const data2 = await fs.readFile(process.argv[3], { encoding: 'utf-8' });
 
-fs.appendFileSync(process.argv[4], data1);
-fs.appendFileSync(process.argv[4], data2);
+  await fs.appendFile(process.argv[4], data1);
+  await fs.appendFile(process.argv[4], data2);
+}
+
+concat();
