239b8bfff9eb3997c28e74c68bf3cb6b333d8aed
Arnthorny <chidubem.anthony26@gmail.com>
2023-10-24 21:53:55 -0700

    Filter by Id and not URL for task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,14 +2,15 @@
 const request = require('request');
 
 const url = process.argv[2];
-const wedgeAntillesUrl = 'https://swapi-api.alx-tools.com/api/people/18/';
 
 request(url, (_, response, body) => {
   // Parse JSON string
   const resJson = JSON.parse(body);
   let count = 0;
   for (const result of resJson.results) {
-    if (result.characters.includes(wedgeAntillesUrl)) count++;
+    result.characters.forEach((cha) => {
+      count = cha.includes(18) ? ++count : count;
+    });
   }
   console.log(count);
 });
